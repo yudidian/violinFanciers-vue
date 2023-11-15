33b70cd6f6d98f59fcf00a4af54f9cf845f2b1be
denghongyu <1848761082@qq.com>
2023-11-15 15:40:16 +0800

    feat: 优化配置

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+declare module '*.vue' {
+	import { DefineComponent } from 'vue';
+	const component: DefineComponent<NonNullable<unknown>, NonNullable<unknown>, any>;
+	export default component;
+}
